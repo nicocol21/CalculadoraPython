1466ca6b9c4bd0daefe022eadf697edc467477bf
nicocol21 <nicolcastillo21j@gmail.com>
2025-09-13 14:32:24 -0500

    Agregar función de potencia

--- Calculadora.py
+++ Calculadora.py
@@ -13,8 +13,13 @@ def dividir (a: float, b: float) -> float:
 def potencia (a: float, b: float) -> float:
     return a ** b
 
-if __name__ == "__main__"
+def elevar (a: float) -> float:
+    return a ** 3
+
+if __name__ == "__main__":
     print(sumar(2,5))
     print(restar(8,3))
     print(multiplicar(9,4))
     print(dividir(12,6))
+    print(potencia(3,3))
+    print(elevar(5))
