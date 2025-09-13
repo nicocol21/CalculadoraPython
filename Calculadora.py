9b592578899bc61e4f64abf0223ad323bc4a1092
nicocol21 <nicolcastillo21j@gmail.com>
2025-09-13 13:43:15 -0500

    Agregando el código para la calculadora básica

---
+++ Calculadora.py
@@ -0,0 +1,20 @@
+def sumar (a: float, b: float) -> float:
+    return a + b
+
+def restar (a: float, b: float) -> float:
+    return a - b
+
+def multiplicar (a: float, b: float) -> float:
+    return a * b
+
+def dividir (a: float, b: float) -> float:
+    return a / b
+
+def potencia (a: float, b: float) -> float:
+    return a ** b
+
+if __name__ == "__main__"
+    print(sumar(2,5))
+    print(restar(8,3))
+    print(multiplicar(9,4))
+    print(dividir(12,6))
